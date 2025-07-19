fc4e2c6dc6715875f422fa055fbe612800c73a4a
ahmad <b-9k@outlook.com>
2025-07-19 10:10:44 -0400

    conclusion of cronjobs test

--- cron/startup.sh
+++ cron/startup.sh
@@ -6,6 +6,14 @@ DISPLAY=':0.0'
 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$id/bus
 
 /usr/bin/notify-send \
-  --expire-time 3000 \
-  --urgency critical "STARTUP COMPLETE" \
+  --expire-time 4000 \
+  --urgency normal \
+  "STARTUP COMPLETE" \
   "I am $HOME/bin/startup and I have been executed thanks to crontab @reboot $HOME/bin/startup.sh"
+
+sleep 0.1
+tput bel
+sleep 0.5
+tput bel
+sleep 0.5
+tput bel
