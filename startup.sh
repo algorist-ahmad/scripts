c24aea04bf0c8f8e7f9e07ab20545514809153e3
ahmad <b-9k@outlook.com>
2025-07-19 09:53:52 -0400

    cronjob example

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/usr/bin/notify-send --urgency critical "STARTUP COMPLETE" "I am $HOME/bin/startup and I have been executed thanks to crontab @reboot $HOME/bin/startup.sh"
+id=1000 #$(id -u)
+DISPLAY=':0.0'
+# XDG_RUNTIME_DIR=/run/user/$id # Must be passed before command
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$id/bus
+
+/usr/bin/notify-send \
+  --expire-time 3000 \
+  --urgency critical "STARTUP COMPLETE" \
+  "I am $HOME/bin/startup and I have been executed thanks to crontab @reboot $HOME/bin/startup.sh"
